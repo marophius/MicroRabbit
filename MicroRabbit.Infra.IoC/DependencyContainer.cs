b325a0211cf979480062e629d7c64406cf274766
marophius <ifeme08@gmail.com>
2022-07-09 03:53:00 -0300

    Initial configurations of microservices

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,8 +18,15 @@ namespace MicroRabbit.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
-            services.AddTransient<IEventBus, RabbitMQBus>();
+            services.AddScoped<IEventBus, RabbitMQBus>();
 
+            // Application Services
+            services.AddScoped<IAccountService, AccountService>();
+
+
+            // Data
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<BankingDbContext>();
         }
     }
 }
